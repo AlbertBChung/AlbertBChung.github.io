4dae3cfcb23f17fd7897d11a788c55fc578cb600
Albert Chung <albertc435@gmail.com>
2017-04-04 02:11:12 -0400

    rework iteration 1

--- js/mainController.js
+++ js/mainController.js
@@ -21,3 +21,86 @@ app.controller('MainController', ['$scope', 'skills', 'clubs', 'work', 'bio','ho
 
 }]);
 
+app.directive('customHover', function () {
+    return {
+        link: function (scope, element, attr) {
+            $(element).hover(
+            function () {
+              if($(window).width()<800 && $(this).attr('class') == 'work-entry ng-scope'){
+                if($( this ).find('h2').text() == 'Riverside Research, Lizzi Center For Biomedical Engineering'){
+                  $( this ).append('<ul>')
+                  for(var i=0; i<scope.workList.work[0].descriptionList.length; i++){
+                    $( this ).append('<li>'+scope.workList.work[0].descriptionList[i]+'</li>')
+                  }
+                  $( this ).append('</ul>').find('li').css('font-size', '18px')
+                }
+                else if ($( this ).find('h2').text() == 'Cornell Engineering World Health Project Team'){
+                  $( this ).append('<ul>')
+                  for(var i=0; i<scope.workList.work[1].descriptionList.length; i++){
+                    $( this ).append('<li>'+scope.workList.work[1].descriptionList[i]+'</li>')
+                  }
+                  $( this ).append('</ul>').find('li').css('font-size', '18px')
+                }
+                else if ($( this ).find('h2').text() == 'Course Consultant for Introduction to Computing Using MATLAB CS 1112'){
+                  $( this ).append('<ul>')
+                  for(var i=0; i<scope.workList.work[2].descriptionList.length; i++){
+                    $( this ).append('<li>'+scope.workList.work[2].descriptionList[i]+'</li>')
+                  }
+                  $( this ).append('</ul>').find('li').css('font-size', '18px')
+                }
+                $(this).animate({
+                  height: '80vh'
+                })
+              }else{
+                if($(this).attr('class') == 'work-entry ng-scope'){
+                  var div = $( this ).clone().prop({ id: 'focus'});
+                  var location = $( this ).offset();
+                  div.css({'z-index': '99', position:'absolute', top: location.top, left: location.left, margin: 0})
+                  if(div.find('h2').text() == 'Riverside Research, Lizzi Center For Biomedical Engineering'){
+                    div.append('<ul>')
+                    for(var i=0; i<scope.workList.work[0].descriptionList.length; i++){
+                      div.append('<li>'+scope.workList.work[0].descriptionList[i]+'</li>')
+                    }
+                    div.append('</ul>').find('li').css('font-size', '25px')
+                  }
+                  else if (div.find('h2').text() == 'Cornell Engineering World Health Project Team'){
+                    div.append('<ul>')
+                    for(var i=0; i<scope.workList.work[1].descriptionList.length; i++){
+                      div.append('<li>'+scope.workList.work[1].descriptionList[i]+'</li>')
+                    }
+                    div.append('</ul>').find('li').css('font-size', '25px')
+                  }
+                  else if (div.find('h2').text() == 'Course Consultant for Introduction to Computing Using MATLAB CS 1112'){
+                    div.append('<ul>')
+                    for(var i=0; i<scope.workList.work[2].descriptionList.length; i++){
+                      div.append('<li>'+scope.workList.work[2].descriptionList[i]+'</li>')
+                    }
+                    div.append('</ul>').find('li').css('font-size', '25px')
+                  }
+                  div.appendTo("body")
+                    .animate({
+                      left: '8%',
+                      top: '305vh',
+                      width: '80%',
+                      height: '80vh'
+                    })
+                    $('.work-entry').not('#focus').hide();
+                }
+                else if($(this).attr('class') == 'row'){
+                  $('#focus').remove();
+                  $('.work-entry').not('#focus').show();
+                }
+              }
+            },
+            function () {
+              if($(window).width()<800 && $(this).attr('class') == 'work-entry ng-scope'){
+                $(this).css('z-index', '0')
+                $(this).find('li').remove()
+               $(this).animate({
+                height: '35vh'
+              })
+            }
+          });
+        }
+    }
+});
--- js/scripts.js
+++ js/scripts.js
@@ -36,7 +36,32 @@ $(document).ready(function(){
 		    dots: true,
 		});
 	}
+	var showSchedule = false;
+    $("#schedule").click(function(){
+    	if(!showSchedule){
+    		$("#schedule-pic").animate({
+            	right: '0px'
+        	});
+    	} else{
+    		$("#schedule-pic").animate({
+            	right: '-150vw'
+        	});   		
+    	}
+    	showSchedule = !showSchedule;
+    });
 });
+
+var towerPos = -100;
+var towerDiv = 2*$(window).height();
+window.onscroll = function() {scroll()};
+
+function scroll(){
+	towerPos = 6 - .00005*Math.pow(Math.abs(towerDiv-document.body.scrollTop),2);
+	$('#tower').css("left",towerPos+"%");
+
+}
+
+
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
